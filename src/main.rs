f0ff1bb4c13abdd09e2c2ef83f3b3b091689d4f6
yabamiah <211063265@aluno.unb.br>
2022-07-05 22:17:08 -0300

    ok

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,22 @@ use core::panic::PanicInfo;
 
 // Essa função e chamada quado rolar PÂNICO
 #[panic_handler]
-fn panic(_info : &PanicInfo) -> ! {
-	 loop{}
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	 loop{}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer(i as isize * 2) = byte;
+            *vga_buffer(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
